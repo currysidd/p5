b912a6b228b9a7fe4632be79e421ab65fb86bf92
currysidd <159699021+currysidd@users.noreply.github.com>
2024-06-18 18:52:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,18 @@ function draw() {
     fill("green")
     circle(20,20,50)
     fill("red")
-    rect(40,50,40,50)
+    circle(780,20,50)
+    fill("blue")
+    circle(780,480,50)
+    fill("yellow")
+    circle(20,480,50)
+    fill("orange")
+    rect(40,40,720,20)
+    rect(40,440,720,20)
+    rect(40,40,20,420)
+    rect(740,40,20,420)
     image(video, 50, 50, 700, 400)
+    
     tint(tintcolor)
 }
 function applyFilter() {
